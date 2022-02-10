63f0debe90bcbc21f546549daec510781f47e6a2
ktenzer <keith.tenzer@gmail.com>
2022-02-10 15:46:39 -0800

    hello world post

---
+++ _posts/2022-02-10-hello-world.md
@@ -0,0 +1,24 @@
+--- 
+layout: single
+title:  "Hello World"
+categories:
+- Linux
+tags:
+- Linux
+- Fedora
+- Opensource
+---
+
+![Hello World Image](/assets/2022-02-10/hello_world_md.png)
+## Overview
+This is a hellow world blog
+
+## Hello
+
+## World
+
+## Summary
+This was a hello world blog
+
+(c) 2022 Keith Tenzer
+
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "Hello World",
+        "excerpt":"  Overview  This is a hellow world blog   Hello   World   Summary  This was a hello world blog   (c) 2022 Keith Tenzer   ","categories": ["Linux"],
+        "tags": ["Linux","Fedora","Opensource"],
+        "url": "/linux/hello-world/",
+        "teaser": null
+      }]
